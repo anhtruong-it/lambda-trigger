a119e3955ea2cfb2d373e5ff5a1a4c957c85007d
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-06-09 01:30:33 +1000

    create admin user group

--- amplify/backend/function/triggerfunction/src/add-to-group.js
+++ amplify/backend/function/triggerfunction/src/add-to-group.js
@@ -1,50 +1,72 @@
-const { wait } = require('@testing-library/user-event/dist/utils');
+// const aws = require('aws-sdk');
+// exports.handler = async (event, context, callback) => {
+//     const cognitoProvider = new
+//         aws.CognitoIdentityServiceProvider({
+//             apiVersion: '2016-04-18'
+//         });
+//     let isAdmin = false
+//     const adminEmails = ['nat002911@gmail.com', 'truongnguyen4042@gmail.com']
+//     // If the user is one of the admins, set the isAdmin variable to true
+//     if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+//         isAdmin = true
+//     }
+//     const groupParams = {
+//         UserPoolId: event.userPoolId,
+//     }
+//     const userParams = {
+//         UserPoolId: event.userPoolId,
+//         Username: event.userName,
+//     }
+//     if (isAdmin) {
+//         groupParams.GroupName = 'Admin'
+//         userParams.GroupName = 'Admin'
+//         // First check to see if the group exists, and if not create the group
+//         try {
+//             await cognitoProvider.getGroup(groupParams).promise();
+//         } catch (e) {
+//             await cognitoProvider.createGroup(groupParams).promise();
+//         }
+//         // If the user is an administrator, place them in the Admin group
+//         try {
+//             await cognitoProvider.adminAddUserToGroup(userParams).promise();
+//             callback(null, event);
+//         } catch (e) {
+//             callback(e);
+//         }
+//     } else {
+//         // If the user is in neither group, proceed with no action
+//         callback(null, event)
+//     }
+// }
 const aws = require('aws-sdk');
 
-exports.handler = async (event, context, callback) => {
-    const cognitoProvider = new
-        aws.CognitoIdentityServiceProvider({
-            apiVersion: '2016-04-18'
-    });
+exports.handler = async (event) => {
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
 
-    let isAdmin = false
-    const adminEmails = ['nat002911@gmail.com']
+  const adminEmails = ['nat002911@gmail.com'];
+  //const adminEmails = 'nat002911@gmail.com';
+  const userEmail = event.request.userAttributes.email;
 
-    // if ther user is one of the admins, set the is Admin variable to true
-    if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
-        isAdmin = true
-    }
-
+  if (adminEmails.includes(userEmail)) {
     const groupParams = {
-        UserPoolId: event.userPoolId,
-    }
+      UserPoolId: event.userPoolId,
+      GroupName: ['Admin']
+    };
 
     const userParams = {
-        UserPoolId: event.userPoolId,
-        Username: event.userName,
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+      GroupName: ['Admin']
+    };
+
+    try {
+      await cognitoProvider.adminAddUserToGroup(userParams).promise();
+      console.log("email check")
+    } catch (error) {
+      console.error('Error adding user to Admin group:', error);
+      throw error;
     }
+  }
 
-    if (isAdmin) {
-        groupParams.GroupName = 'Admin';
-        userParams.GroupName = 'Admin';
-
-
-        // first check to see if the group exists, and if not create the group
-        try {
-            await cognitoProvider.getGroup(groupParams).promise();
-        } catch (e) {
-            await cognitoProvider.createGroup(groupParams).promise();
-        }
-
-        // If the user is an administrater, place them in the admin group
-        try {
-            await cognitoProvider.adminAddUserToGroup(userParams).promise();
-            callback(null, event);
-        } catch (e) {
-            callback(e);
-        }
-    } else {
-        // If ther user is in neither group, proceed with no action
-        callback(null, event)
-    }
-}
+  return event;
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect, useState} from "react";
+// import { Auth } from 'aws-amplify';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import './App.css';
+// import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+// function App({ signOut }) {
+//   const [user, updateUser] = useState(null);
+//   useEffect(() => {
+//     Auth.currentAuthenticatedUser()
+//     .then(user => updateUser(user))
+//     .catch(err => console.log(err));
+//   }, [])
+//   let isAdmin = false
+//   if (user) {
+//     const { signInUserSession: { idToken: { payload }} } = user
+//     console.log('payload: ', payload);
+//     if ( payload['cognito:groups'] && payload['cognito:groups'].includes('Admin')) {
+//       isAdmin = true
+//     }
+//     console.log('admin: ', isAdmin);
+//   }
+
+//   return (
+//     <div className="App">
+//       <header>
+//         <h1> Hello World</h1>
+//         { isAdmin && <p>Welcome, Admin</p> }
+//       </header>
+//       <button onClick={signOut}>Sign out</button>
+//     </div>
+//   )
+// }
+
+// export default withAuthenticator(App)
+
+import React, { useEffect, useState } from "react";
+import { Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+
+function App({ signOut }) {
+  const [user, updateUser] = useState(null);
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => updateUser(user))
+      .catch(err => console.log(err));
+  }, []);
+
+  let isAdmin = false;
+  if (user) {
+    const { signInUserSession: { idToken: { payload } } } = user;
+    console.log('payload:', payload); // Add this line to log the payload object
+    if (payload['cognito:groups'] && payload['cognito:groups'].includes('Admin')) {
+      isAdmin = true;
+    }
+    console.log('isAdmin:', isAdmin); // Add this line to log the value of isAdmin
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hello World</h1>
+        {isAdmin && <p>Welcome, Admin</p>}
       </header>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
